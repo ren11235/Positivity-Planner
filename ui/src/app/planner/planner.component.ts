ebdc9b9b485b82547b6b55889ed3b40c7d39aaa4
Miranda Overstreet <mira.overstreet@gmail.com>
2022-04-05 00:02:02 -0400

    Added ability to drag to create events

--- ui/src/app/planner/planner.component.ts
+++ ui/src/app/planner/planner.component.ts
@@ -1,19 +1,65 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Injectable, OnInit, ViewEncapsulation, ChangeDetectorRef, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
 import { EventService, Event } from '../planner.service';
-import { CalendarEvent, CalendarView } from 'angular-calendar';
+import { CalendarEvent, CalendarEventTitleFormatter, CalendarView } from 'angular-calendar';
+import { addDays, addMinutes, endOfWeek } from 'date-fns';
+import { WeekViewHourSegment } from 'calendar-utils';
+import { fromEvent } from 'rxjs';
+import { finalize, takeUntil } from 'rxjs/operators';
+function floorToNearest(amount: number, precision: number) {
+  return Math.floor(amount / precision) * precision;
+}
+
+function ceilToNearest(amount: number, precision: number) {
+  return Math.ceil(amount / precision) * precision;
+}
+
+@Injectable()
+export class CustomEventTitleFormatter extends CalendarEventTitleFormatter {
+  weekTooltip(event: CalendarEvent, title: string) {
+    if (!event.meta.tmpEvent) {
+      return super.weekTooltip(event, title);
+    }
+  }
+
+  dayTooltip(event: CalendarEvent, title: string) {
+    if (!event.meta.tmpEvent) {
+      return super.dayTooltip(event, title);
+    }
+  }
+}
+
 @Component({
   selector: 'app-event',
   templateUrl: './planner.component.html',
   styleUrls: ['./planner.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+
+  providers: [
+    {
+      provide: CalendarEventTitleFormatter,
+      useClass: CustomEventTitleFormatter,
+    },
+  ],
+  styles: [
+    `
+      .disable-hover {
+        pointer-events: none;
+      }
+    `,
+  ],
+  encapsulation: ViewEncapsulation.None,
 })
-export class EventComponent implements OnInit {
+
+
+export class EventComponent{
   view: CalendarView = CalendarView.Month;
   activeEvents: Event[];
   eventMessage: string;
   eventTime: string;
 
   viewDate = new Date();
+
+  weekStartsOn: 0 = 0;
 
   events: CalendarEvent[] = [
     //{
@@ -29,15 +75,74 @@ export class EventComponent implements OnInit {
   clickedDate: Date;
 
   clickedColumn: number;
+
+  dragToCreateActive = false;
   
-  constructor(private eventService: EventService) { 
-    this.activeEvents = [];
-    this.eventMessage = "";
-    this.eventTime = "";
-    console.log("test");
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  //constructor(private eventService: EventService) { 
+    //this.activeEvents = [];
+    //this.eventMessage = "";
+    //this.eventTime = "";
+    //console.log("test");
+  //}
+
+  startDragToCreate(
+    segment: WeekViewHourSegment,
+    mouseDownEvent: MouseEvent,
+    segmentElement: HTMLElement
+  ) {
+    const dragToSelectEvent: CalendarEvent = {
+      id: this.events.length,
+      title: 'New event',
+      start: segment.date,
+      meta: {
+        tmpEvent: true,
+      },
+    };
+    this.events = [...this.events, dragToSelectEvent];
+    const segmentPosition = segmentElement.getBoundingClientRect();
+    this.dragToCreateActive = true;
+    const endOfView = endOfWeek(this.viewDate, {
+      weekStartsOn: this.weekStartsOn,
+    });
+
+    fromEvent(document, 'mousemove')
+      .pipe(
+        finalize(() => {
+          delete dragToSelectEvent.meta.tmpEvent;
+          this.dragToCreateActive = false;
+          this.refresh();
+        }),
+        takeUntil(fromEvent(document, 'mouseup'))
+      )
+      .subscribe((mouseMoveEvent: MouseEvent) => {
+        const minutesDiff = ceilToNearest(
+          mouseMoveEvent.clientY - segmentPosition.top,
+          30
+        );
+
+        const daysDiff =
+          floorToNearest(
+            mouseMoveEvent.clientX - segmentPosition.left,
+            segmentPosition.width
+          ) / segmentPosition.width;
+
+        const newEnd = addDays(addMinutes(segment.date, minutesDiff), daysDiff);
+        if (newEnd > segment.date && newEnd < endOfView) {
+          dragToSelectEvent.end = newEnd;
+        }
+        this.refresh();
+      });
   }
 
-  ngOnInit() {
+  private refresh() {
+    this.events = [...this.events];
+    this.cdr.detectChanges();
+  }
+
+
+  /*ngOnInit() {
     this.getAll();
   }
 
@@ -48,6 +153,16 @@ export class EventComponent implements OnInit {
   }
 
   addNewEvent() {
+    //var event: CalendarEvent =
+      //{
+        //title: 'Has custom class',
+        //color: {
+         // primary: '#ad2121',
+        // // secondary: '#FAE3E3',
+       //},
+       // start: new Date(),
+        //cssClass: 'my-custom-class',
+      //};
     console.log("We are adding an event");
     var newEvent : Event = {
       name: this.eventMessage,
@@ -74,4 +189,5 @@ export class EventComponent implements OnInit {
   }
 }
 
-
+*/
+}
